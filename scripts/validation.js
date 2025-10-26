6c6fc9626dc2b8499c79cb2296073a06e21ada66
xDren98 <dreenhd@gmail.com>
2025-10-26 22:36:39 +0100

    Create validation.js

---
+++ scripts/validation.js
@@ -0,0 +1,18 @@
+function validaCodiceFiscale(cf) {
+  if (!cf) return false;
+  const regex = /^[A-Z0-9]{16}$/i;
+  return regex.test(cf);
+}
+
+function validaTelefono(tel) {
+  if (!tel) return false;
+  const regex = /^[0-9\+\-\s]{7,15}$/;
+  return regex.test(tel);
+}
+
+function validaNomeCognome(nome) {
+  if (!nome) return false;
+  return nome.length > 2;
+}
+
+export { validaCodiceFiscale, validaTelefono, validaNomeCognome };
